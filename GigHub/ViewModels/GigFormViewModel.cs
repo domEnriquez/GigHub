c7fc4eb23d8ea92cb3bcd0ce1cec27669c21fe23
Dominic <dominicjosephenriquez@gmail.com>
2018-05-03 05:47:13 +0800

    Added GigFormViewModel

---
+++ GigHub/ViewModels/GigFormViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace GigHub.ViewModels
+{
+    public class GigFormViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+
+    }
+}
