16034ceaecb1b4c5d9ddd4581083f87556894a0f
Dominic <dominicjosephenriquez@gmail.com>
2018-05-24 07:06:42 +0800

    Mapping domain objects to DTOs

---
+++ GigHub/Controllers/Api/NotificationsController.cs
@@ -0,0 +1,36 @@
+﻿using AutoMapper;
+using GigHub.Dtos;
+using GigHub.Models;
+using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+
+namespace GigHub.Controllers.Api
+{
+    [Authorize]
+    public class NotificationsController : ApiController
+    {
+        private ApplicationDbContext context;
+
+        public NotificationsController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        public IEnumerable<NotificationDto> GetNewNotifications()
+        {
+            string userId = User.Identity.GetUserId();
+            List<Notification> notifications = context.UserNotifications
+                .Where(un => un.UserId == userId && !un.IsRead)
+                .Select(un => un.Notification)
+                .Include(n => n.Gig.Artist)
+                .ToList();
+
+
+
+            return notifications.Select(Mapper.Map<Notification, NotificationDto>);
+        }
+    }
+}
--- GigHub/Dtos/FollowDto.cs
+++ GigHub/Dtos/FollowDto.cs
@@ -1,4 +1,4 @@
-﻿namespace GigHub.Models
+﻿namespace GigHub.Dto
 {
     public class FollowDto
     {
